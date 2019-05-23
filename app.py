4552ebcd21b89b457b5b04ce037d02a82e4d67c0
Brian Hogan <bkhogan2@gmail.com>
2019-05-22 19:10:02 -0700

    show average number of females and males who survived

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 test = pd.read_csv("input/test.csv")
 test_shape = test.shape
@@ -6,4 +7,9 @@ print(test_shape)
 
 train = pd.read_csv("input/train.csv")
 train_shape = train.shape
-print(train.head(10))
+# print(train.head(10))
+
+sex_pivot = train.pivot_table(index="Sex", values="Survived")
+
+sex_pivot.plot.bar()
+plt.show()
